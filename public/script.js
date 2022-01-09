c32c2ad81c45fc7b1eed4f681d2f7491efbe87f9
Spencer Harston <spencer314@gmail.com>
2022-01-08 22:06:00 -0700

    Add ability to set random color (#2)

--- public/script.js
+++ public/script.js
@@ -3,11 +3,24 @@ function setBackgroundColor(color) {
     bodyEl.style.backgroundColor = '#' + color;
 }
 
+function getRandomHexCode(){
+    let letters = '0123456789ABCDEF';
+    let color = '';
+    for(let i = 0; i < 6; i++){
+        color += letters[(Math.floor(Math.random() * 16))];
+    }
+    return color;
+}
+
 function getURLParams() {
     let params = new URLSearchParams(window.location.search);
     
     if (params.has('color')){
-        setBackgroundColor(params.get('color'));
+        if(params.get('color') == 'random'){
+            setBackgroundColor(getRandomHexCode());
+        } else {
+            setBackgroundColor(params.get('color'));
+        }
     }
     else{
         console.log('no color specified');
